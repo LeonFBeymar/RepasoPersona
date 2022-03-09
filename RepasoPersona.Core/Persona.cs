2d9b922e23f5a198e092bfcc9420a25814b2ae26
LeonFBeymar <beymarleonf18@gmail.com>
2022-03-09 15:40:31 -0300

    Creacion Repaso Persona

---
+++ RepasoPersona.Core/Persona.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace RepasoPersona.Core
+{
+    public class Persona
+    {
+        public string Nombre {get; private set;}
+        public string Apellido {get; private set;}
+        public double Efectivo {get; private set;}
+        public Persona (string nombre, string apellido, double efectivo)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            Efectivo = efectivo;
+        }
+        public Persona () => Efectivo = 0;
+        public void Debito(double monto)
+        {
+            if (monto <= 0)
+            {
+                throw new Exception("El monto debe ser > 0");
+            }
+            if (Efectivo < monto)
+            {
+                throw new Exception("Saldo insuficiente");
+            }
+            else Efectivo -= monto;
+        }
+
+        public void Acreditar(double monto)
+        {
+            if(monto <= 0)
+            {
+                throw new Exception("El monto debe ser > 0");
+            }
+            else Efectivo += monto;
+        }
+    }
+}
