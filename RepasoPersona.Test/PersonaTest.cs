caecebcb91273adee2918d03fdc1511e970907ee
luchoxx87 <beymarleonf18@gmail.com>
2022-03-14 17:04:25 -0300

    RepasoPersona

--- RepasoPersona.Test/PersonaTest.cs
+++ RepasoPersona.Test/PersonaTest.cs
@@ -7,13 +7,20 @@ namespace RepasoPersona.Test
     public class PersonaTest
     {
         public Persona Pepito {get; set;}
-        [Fact]
+        public PersonaTest() => Pepito = new Persona("Pepito","Gomez",0);
+        [Fact]//Hecho: cada vez que encuetre uno de estos si o si se va a ejecutar
         public void Constructor()
         {
-            Pepito = new Persona("Pepito","Gomez",0);
             Assert.Equal("Pepito",Pepito.Nombre);
             Assert.Equal("Gomez",Pepito.Apellido);
             Assert.Equal(0,Pepito.Efectivo);
+        }
+        [Fact]
+        public void AcreditarPositivo()
+        {
+            double esperado = 1000;
+            Pepito.Acreditar(1000);
+            Assert.NotEqual(esperado,Pepito.Efectivo,3);
         }
     }
 }
